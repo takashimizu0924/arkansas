bf30e59f80416ac6727a260beb0ce32f5953566a
takashimizu0924 <takashi.924@icloud.com>
2022-01-23 01:16:15 +0900

    RSIタスク修正

--- go_task/src/bybit/mainTask.go
+++ go_task/src/bybit/mainTask.go
@@ -1,11 +1,55 @@
 package bybit
 
-import "log"
+import (
+	"log"
+)
 
-func (b *Bybit) CryptTaskV1(symbol string) {
-	var tickerChannel = make(chan string)
-	go b.BBandSignalTask(symbol, tickerChannel)
-	for chanel := range tickerChannel {
-		log.Println(chanel)
+func (b *Bybit) CryptTaskV1() {
+	signalCh := make(chan Signal)
+	var signals []Signal
+	coins := b.GetAllCoin("USDT")
+	// for i := 0; i < len(coins); i++ {
+	// 	go b.BBandSignalTask(coins[i], signalCh)
+	// 	// go b.MacdSignalTask(coins[i])
+	// 	// go b.RsiSignalTask(coins[i])
+	// 	// result := <-signalCh
+	// 	// signals = append(signals, result)
+	// 	// log.Println(signals)
+	// }
+	var upList []Signal
+	var downList []Signal
+	var targetCoin []Signal
+	go b.BBandSignalTask(coins, signalCh)
+	go b.MacdSignalTask(coins, signalCh)
+	go b.RsiSignalTask(coins, signalCh)
+	for signal := range signalCh {
+		signals = append(signals, signal)
+		if signal.Sign == 0 {
+			downList = append(downList, signal)
+			if len(downList) > 0 {
+				for i := 0; i < len(downList); i++ {
+					if downList[i].CoinPair == signal.CoinPair {
+						targetCoin = append(targetCoin, signal)
+					}
+				}
+			}
+		}
+		if signal.Sign == 1 {
+			upList = append(upList, signal)
+			if len(upList) > 0 {
+				for i := 0; i < len(upList); i++ {
+					if upList[i].CoinPair == signal.CoinPair {
+						log.Println(targetCoin)
+						targetCoin = append(targetCoin, signal)
+					}
+				}
+			}
+		}
 	}
+
+	log.Println("Signals:", signals)
+	log.Println("Up:", upList)
+	log.Println("Down:", downList)
+	log.Println(targetCoin)
+	log.Println("タスク終了")
 }
--- go_task/src/bybit/subTask.go
+++ go_task/src/bybit/subTask.go
@@ -2,62 +2,246 @@ package bybit
 
 import (
 	"log"
+	"strings"
 	"time"
 )
 
-func (b *Bybit) BBandSignalTask(symbol string, ch chan<- string) {
-	for {
-		before200Min := time.Now().Add(-200 * time.Hour).Unix()
-		candles, err := b.GetKline(symbol, "60", before200Min, 0)
+type Signal struct {
+	Type     string
+	CoinPair string
+	Sign     int
+}
+
+func (b *Bybit) GetBefore200LinearKline(symbol, interval string, from time.Duration) (candles []LinearOHLC, err error) {
+	before200time := time.Now().Add(-200 * from).Unix()
+	candles, err = b.GetLinearKline(symbol, interval, before200time, 0)
+	if err != nil {
+		return nil, err
+	}
+	return candles, nil
+}
+
+/*-------------------------------ボリンジャーバンド----------------------------------------------*/
+
+// func (b *Bybit) BBandSignalTask(symbol string) (coin string, sign int) {
+// 	candles, err := b.GetBefore200LinearKline(symbol, "5", time.Minute)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	var closes []float64
+// 	for i := 0; i < len(candles); i++ {
+// 		closes = append(closes, candles[i].Close)
+// 	}
+// 	up, mid, down := BBand(closes, 20, 2)
+// 	ticker, err := b.Ticker(symbol)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+
+// 	if len(up) == 0 || len(mid) == 0 || len(down) == 0 {
+// 		return "", 999
+// 	}
+// 	for _, tick := range ticker {
+// 		if tick.LastPrice >= up[len(up)-1] {
+// 			return symbol, 0 //下降予想
+// 		}
+// 		if tick.LastPrice <= down[len(down)-1] {
+// 			return symbol, 1 //上昇予想
+// 		}
+// 	}
+// 	return "", 999
+// }
+func (b *Bybit) BBandSignalTask(symbols []string, signal chan<- Signal) {
+	for _, symbol := range symbols {
+		defer close(signal)
+		candles, err := b.GetBefore200LinearKline(symbol, "5", time.Minute)
 		if err != nil {
 			log.Println(err)
 		}
 		var closes []float64
-		for _, v := range candles {
-			closes = append(closes, v.Close)
+		for i := 0; i < len(candles); i++ {
+			closes = append(closes, candles[i].Close)
 		}
-		//ボリンジャーバンドを作成し、現在の価格がどの位置にあるかを調べる
-		//今のところ出来ている 1/8 pm10:32
 		up, mid, down := BBand(closes, 20, 2)
-
 		ticker, err := b.Ticker(symbol)
+		if err != nil {
+			log.Println(err)
+		}
+		if len(up) == 0 || len(mid) == 0 || len(down) == 0 {
+			return
+		}
 		for _, tick := range ticker {
-			if tick.LastPrice >= mid[len(mid)-1] {
-				sign := downTrend(up[len(up)-1], tick.LastPrice)
-				if sign {
-					ch <- "下降サインが出ています"
-				} else {
-					ch <- "平均線より上にありますが、サインは出ていません"
+			if tick.LastPrice >= up[len(up)-1] {
+				signal <- Signal{
+					Type:     "BBand",
+					CoinPair: symbol,
+					Sign:     0,
 				}
 			}
-			if tick.LastPrice <= mid[len(mid)-1] {
-				sign := upTrend(down[len(down)-1], tick.LastPrice)
-				if sign {
-					ch <- "上昇サインが出ています"
-				} else {
-					ch <- "平均線より下にありますが、サインは出ていません"
+			if tick.LastPrice <= down[len(down)-1] {
+				signal <- Signal{
+					Type:     "BBand",
+					CoinPair: symbol,
+					Sign:     1,
 				}
 			}
 		}
 	}
 }
 
-func upTrend(downBand, price float64) bool {
-	if downBand >= price {
-		return true
+/*-------------------------------------MACD-------------------------------------------------*/
+
+// func (b *Bybit) MacdSignalTask(symbol string) (string, int) {
+// 	candles, err := b.GetBefore200LinearKline(symbol, "5", time.Minute)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	var closes []float64
+// 	for i := 0; i < len(candles); i++ {
+// 		closes = append(closes, candles[i].Close)
+// 	}
+// 	macd, macdSignal, macdHist := Macd(closes, 12, 26, 9)
+// 	if len(macd) == 0 || len(macdSignal) == 0 || len(macdHist) == 0 {
+// 		return "データが収集出来ていません", 999
+// 	}
+// 	if macd[len(macd)-1] > 0 && macdHist[len(macdHist)-1] > 0 && macd[len(macd)-1] > macdSignal[len(macdSignal)-1] {
+// 		return symbol, 3 //"MACDにより強気サインが出ています"
+// 	}
+// 	if macd[len(macd)-1] < 0 && macdHist[len(macdHist)-1] < 0 && macd[len(macd)-1] < macdSignal[len(macdSignal)-1] {
+// 		return symbol, 2 //"MACDにより弱気サインが出ています"
+// 	}
+// 	if macd[len(macd)-1] < 0 && macdHist[len(macdHist)-1] > 0 && macd[len(macd)-1] > macdSignal[len(macdSignal)-1] {
+// 		// rsi := b.RsiSignalTask(closes)
+// 		// if rsi == 1 {
+// 		// 	return symbol, 1 //"上昇の可能性があるためこの通貨を監視する"
+// 		// }
+// 		return symbol, 1 //"上昇の可能性があるためこの通貨を監視する"
+// 	}
+// 	if macd[len(macd)-1] > 0 && macdHist[len(macdHist)-1] < 0 && macd[len(macd)-1] < macdSignal[len(macdSignal)-1] {
+// 		// rsi := b.RsiSignalTask(closes)
+// 		// if rsi == 0 {
+// 		// 	return symbol, 0 //"下降の可能性があるためこの通貨を監視する"
+// 		// }
+// 		return symbol, 0 //"下降の可能性があるためこの通貨を監視する"
+// 	}
+// 	return "相場情報取得出来ていません", 999
+// }
+func (b *Bybit) MacdSignalTask(symbols []string, signal chan<- Signal) {
+	for _, symbol := range symbols {
+		candles, err := b.GetBefore200LinearKline(symbol, "5", time.Minute)
+		if err != nil {
+			log.Println(err)
+		}
+		var closes []float64
+		for i := 0; i < len(candles); i++ {
+			closes = append(closes, candles[i].Close)
+		}
+		macd, macdSignal, macdHist := Macd(closes, 12, 26, 9)
+		if len(macd) == 0 || len(macdSignal) == 0 || len(macdHist) == 0 {
+			return
+		}
+		if macd[len(macd)-1] > 0 && macdHist[len(macdHist)-1] > 0 && macd[len(macd)-1] > macdSignal[len(macdSignal)-1] {
+			signal <- Signal{
+				Type:     "MACD",
+				CoinPair: symbol,
+				Sign:     3,
+			} //"MACDにより強気サインが出ています"
+		}
+		if macd[len(macd)-1] < 0 && macdHist[len(macdHist)-1] < 0 && macd[len(macd)-1] < macdSignal[len(macdSignal)-1] {
+			signal <- Signal{
+				Type:     "MACD",
+				CoinPair: symbol,
+				Sign:     2,
+			} //"MACDにより弱気サインが出ています"
+		}
+		if macd[len(macd)-1] < 0 && macdHist[len(macdHist)-1] > 0 && macd[len(macd)-1] > macdSignal[len(macdSignal)-1] {
+			// rsi := b.RsiSignalTask(closes)
+			// if rsi == 1 {
+			// 	return symbol, 1 //"上昇の可能性があるためこの通貨を監視する"
+			// }
+			signal <- Signal{
+				Type:     "MACD",
+				CoinPair: symbol,
+				Sign:     1,
+			} //"上昇の可能性があるためこの通貨を監視する"
+		}
+		if macd[len(macd)-1] > 0 && macdHist[len(macdHist)-1] < 0 && macd[len(macd)-1] < macdSignal[len(macdSignal)-1] {
+			// rsi := b.RsiSignalTask(closes)
+			// if rsi == 0 {
+			// 	return symbol, 0 //"下降の可能性があるためこの通貨を監視する"
+			// }
+			signal <- Signal{
+				Type:     "MACD",
+				CoinPair: symbol,
+				Sign:     0,
+			} //"下降の可能性があるためこの通貨を監視する"
+		}
 	}
-	if downBand <= price && downBand/price*100 > 99.8 {
-		return true
-	}
-	return false
 }
 
-func downTrend(upBand, price float64) bool {
-	if upBand <= price {
-		return true
+/*------------------------------------------RSI---------------------------------------------------------*/
+
+// func (b *Bybit) RsiSignalTask(symbol string) int {
+// 	candles, err := b.GetBefore200LinearKline(symbol, "5", time.Minute)
+// 	if err != nil {
+// 		log.Println(err)
+// 	}
+// 	var closes []float64
+// 	for i := 0; i < len(candles); i++ {
+// 		closes = append(closes, candles[i].Close)
+// 	}
+// 	rsi := RSI(closes, 9)
+// 	if rsi[len(rsi)-1] >= 70 {
+// 		return 0
+// 	} else if rsi[len(rsi)-1] <= 30 {
+// 		return 1
+// 	} else {
+// 		return 2
+// 	}
+// }
+func (b *Bybit) RsiSignalTask(symbols []string, signal chan<- Signal) {
+	for _, symbol := range symbols {
+		candles, err := b.GetBefore200LinearKline(symbol, "5", time.Minute)
+		if err != nil {
+			log.Println(err)
+		}
+		var closes []float64
+		for i := 0; i < len(candles); i++ {
+			closes = append(closes, candles[i].Close)
+		}
+		rsi := RSI(closes, 9)
+		if rsi[len(rsi)-1] >= 70 {
+			signal <- Signal{
+				Type:     "RSI",
+				CoinPair: symbol,
+				Sign:     0,
+			}
+		}
+		if rsi[len(rsi)-1] <= 30 {
+			signal <- Signal{
+				Type:     "RSI",
+				CoinPair: symbol,
+				Sign:     1,
+			}
+		}
 	}
-	if upBand >= price && price/upBand*100 > 99.8 {
-		return true
-	}
-	return false
 }
+
+/*----------------------------USDTの通貨ペアのコインを全部取得する------------------------------------------------*/
+func (b *Bybit) GetAllCoin(currency string) (coins []string) {
+	allcoins, _ := b.GetSymbols()
+	for _, coin := range allcoins {
+		b := strings.Contains(coin.Name, "USDT")
+		if b {
+			coins = append(coins, coin.Name)
+		}
+	}
+	return
+}
+
+//TODO
+/*
+ある一定時間の間に平均線を超えなかったら損切りを行う処理を実装
+多分２〜４時間ぐらい
+そのプッシュ通知も実装する
+*/
