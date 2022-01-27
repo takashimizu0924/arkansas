1c63dda47b22dc40a3acff58ecd907f20f45540a
VastPureWater <hiroshi222432@gmail.com>
2022-01-27 21:34:52 +0900

    go_task削除

--- arkansas/app/asset/bybit.py
+++ arkansas/app/asset/bybit.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python3
+import ccxt
+
 
 class Bybit:
     """Bybitクラス
     """
-    def __init__(self):
+    def __init__(self, apikey, secret, urls):
+        """コンストラクタ
+
+        Args:
+            apikey (str): apiキー文字列
+            secret (str): apiシークレットキー文字列
+            urls (str): apiのURL文字列
+        """
+        self.bybit_api_inf              = {}
+        self.bybit_api_inf["apikey"]    = apikey
+        self.bybit_api_inf["secret"]    = secret
+        self.bybit_api_inf["urls"]      = {"api":urls}
+        self.bybit_api                  = ccxt.bybit(self.bybit_api_inf)
+        
+    def get_position(self):
         pass
+        
