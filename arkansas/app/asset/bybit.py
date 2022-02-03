41b4651724776e4dd448719e579879a7f42317ff
VastPureWater <hiroshi222432@gmail.com>
2022-02-04 00:14:01 +0900

    シンボルクラス作成、シンボルテーブル(仮)作成

--- arkansas/app/asset/bybit.py
+++ arkansas/app/asset/bybit.py
@@ -1,26 +1,175 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 import ccxt
+
+
+class BybitSymbol:
+    def __init__(self):
+        self.__symbol_table = {
+                                "USDT":[
+                                    "BIT/USDT",
+                                    "BTC/USDT",
+                                    "ETH/USDT",
+                                    "MANA/USDT",
+                                    "SAND/USDT",
+                                    "SHIB1000/USDT",
+                                    "ADA/USDT",
+                                    "BNB/USDT",
+                                    "XRP/USDT",
+                                    "SOL/USDT",
+                                    "DOT/USDT",
+                                    "DOGE/USDT",
+                                    "UNI/USDT",
+                                    "LUNA/USDT",
+                                    "AVAX/USDT",
+                                    "LINK/USDT",
+                                    "LTC/USDT",
+                                    "ALGO/USDT",
+                                    "BCH/USDT",
+                                    "ATOM/USDT",
+                                    "MATIC/USDT",
+                                    "FIL/USDT",
+                                    "ICP/USDT",
+                                    "ETC/USDT",
+                                    "XLM/USDT",
+                                    "VET/USDT",
+                                    "AXS/USDT",
+                                    "TRX/USDT",
+                                    "FTT/USDT",
+                                    "XTZ/USDT",
+                                    "THETA/USDT",
+                                    "HBAR/USDT",
+                                    "EOS/USDT",
+                                    "AAVE/USDT",
+                                    "NEAR/USDT",
+                                    "FTM/USDT",
+                                    "KSM/USDT",
+                                    "OMG/USDT",
+                                    "IOTX/USDT",
+                                    "DASH/USDT",
+                                    "COMP/USDT",
+                                    "ONE/USDT",
+                                    "CHZ/USDT",
+                                    "LRC/USDT",
+                                    "ENJ/USDT",
+                                    "XEM/USDT",
+                                    "SUSHI/USDT",
+                                    "DYDX/USDT",
+                                    "SRM/USDT",
+                                    "CRV/USDT",
+                                    "IOST/USDT",
+                                    "CELR/USDT",
+                                    "CHR/USDT",
+                                    "WOO/USDT",
+                                    "ALICE/USDT",
+                                    "ENS/USDT",
+                                    "C98/USDT",
+                                    "GALA/USDT",
+                                    "KEEP/USDT",
+                                    "SLP/USDT",
+                                    "ANT/USDT",
+                                    "ROSE/USDT",
+                                    "CROU/USDT",
+                                    "TFI/USDT",
+                                    "KNC/USDT",
+                                    "SXP/USDT",
+                                    "AR/USDT",
+                                    "STORJ/USDT",
+                                    "SPELL/USDT",
+                                    "BSV/USDT",
+                                    "CREAM/USDT",
+                                    "COTI/USDT",
+                                    "1INCHI/USDT",
+                                    "TLM/USDT",
+                                    "EGLD/USDT",
+                                    "PEOPLE/USDT",
+                                    "FLOW/USDT",
+                                    "COMP/USDT",
+                                    "CELOU/USDT",
+                                    "BAT/USDT",
+                                    "ZEN/USDT",
+                                    "STX/USDT",
+                                    "SNX/USDT",
+                                    "XMR/USDT",
+                                    "SRM/USDT",
+                                    "DUSK/USDT",
+                                    "WAVES/USDT",
+                                    "YGG/USDT",
+                                    "DENT/USDT",
+                                    "IOTA/USDT",
+                                    "RVN/USDT",
+                                    "ANKR/USDT",
+                                    "KAVA/USDT",
+                                    "RNDRU/USDT",
+                                    "RUNE/USDT",
+                                    "ILVU/USDT",
+                                    "QTUM/USDT",
+                                    "CTK/USDT",
+                                    "ZEC/USDT",
+                                    "BTT/USDT",
+                                    "LOOKS/USDT",
+                                    "KLAY/USDT",
+                                    "SFP/USDT",
+                                    "GTC/USDT",
+                                    "REQ/USDT",
+                                    "GRT/USDT",
+                                    "MASK/USDT",
+                                    "AUDIO/USDT",
+                                    "LPY/USDT",
+                                    "IMX/USDT",
+                                    "HNT/USDT",
+                                    "CVC/USDT",
+                                    "JASMY/USDT",
+                                    "BICO/USDT",
+                                    "RSR/USDT",
+                                    "REN/USDT",
+                                    "LIT/USDT",
+                                    "SCU/USDT",
+                                    ]
+                                }
+        def get_name_from_id(self, symbol_type, symbol_id):
+            """ID番号から通貨種別名を取得する
+
+            Args:
+                symbol_type (str): 通貨種別
+                symbol_id (int): [description]
+
+            Returns:
+                [type]: [description]
+            """
+            if not self.__symbol_table[symbol_type] or not self.__symbol_table[symbol_type][symbol_id]:
+                return ""
+            return self.__symbol_table[symbol_type][symbol_id]
+            
 
 
 class Bybit:
     """Bybitクラス
     """
-    def __init__(self, apikey, secret, urls):
+    def __init__(self):
         """コンストラクタ
-
-        Args:
-            apikey (str): apiキー文字列
-            secret (str): apiシークレットキー文字列
-            urls (str): apiのURL文字列
+            NOTE: bybitAPIを使用する事前準備
         """
-        self.bybit_api_inf              = {}
-        self.bybit_api_inf["apikey"]    = apikey
-        self.bybit_api_inf["secret"]    = secret
-        self.bybit_api_inf["urls"]      = {"api":urls}
-        self.bybit_api                  = ccxt.bybit(self.bybit_api_inf)
+        self.bybit_api = ccxt.bybit()
+
+    # def __init__(self, apikey, secret, urls):
+    #     """コンストラクタ
+
+    #     Args:
+    #         apikey (str): apiキー文字列
+    #         secret (str): apiシークレットキー文字列
+    #         urls (str): apiのURL文字列
+    #     """
+    #     self.bybit_api_inf              = {}
+    #     self.bybit_api_inf["apiKey"]    = apikey
+    #     self.bybit_api_inf["secret"]    = secret
+    #     self.bybit_api_inf["urls"]      = {"api":urls}  # 不要情報？
+    #     # self.bybit_api                  = ccxt.bybit(self.bybit_api_inf)
+    #     self.bybit_api                  = ccxt.bybit()
         
     def fetch_ticker(self,symbol):
         """ティッカー情報を取得する
+            NOTE:BybitのPublicAPI
 
         Args:
             symbol (str): 取得するティッカーシンボルの指定
@@ -31,3 +180,7 @@ class Bybit:
         if not symbol == "":
             ticker = self.bybit_api.fetch_ticker(symbol)
         return ticker
+    
+    # def fetch_my_trades(self, symbol):
+    #     trades = self.bybit_api.fetch_my_trades(symbol)
+    #     return trades
